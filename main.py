3b279ec7a6def0058fcaad892a474604094554f0
hindmasj <hindmasj@users.noreply.github.com>
2024-02-23 12:53:00 +0000

    Made a start by adapting the main.py and setting unix style lines and shebang.

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Energy Tariff Analysis
+# Calculate tariff costs based on usage and export data stored in Elasticsearch
+
+# This script is adapted from the PyCharm sample main.py.
+# Press Shift+F10 to execute it.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Press the green button in the gutter to run the script.
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == '__main__':
+    print('Welcome to the Energy Tariff Analysis script')
