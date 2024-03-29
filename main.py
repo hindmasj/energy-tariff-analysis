3611d13b748491ea022daace1264118dbcde66fc
hindmasj <hindmasj@users.noreply.github.com>
2024-03-29 15:37:27 +0000

    Re-organised into classes

--- main.py
+++ main.py
@@ -12,45 +12,68 @@ import argparse
 import configparser
 import sys
 
-DFLT_CONF_FILE='eta.conf'
-DFLT_ES_TOKEN_FILE='es_token'
 
-SECT_ES='elasticsearch'
-OPT_ES_TOKEN='token_file'
+class Configuration:
+    DFLT_CONF_FILE = 'eta.conf'
+    DFLT_ES_TOKEN_FILE = 'es_token'
 
+    SECT_ES = 'elasticsearch'
+    OPT_ES_TOKEN = 'token_file'
 
-def parse_command_line(argv):
-    parser = argparse.ArgumentParser(
-        prog="Energy Tariff Analysis",
-        description="Analyse energy tariffs against consumption and generation records",
-        epilog="Load records into Elasticsearch"
-    )
-    parser.add_argument('-c', '--config', default=DFLT_CONF_FILE)
-    return parser.parse_args()
+    def __init__(self,argv):
+        self.argv=argv
+        self.args=self.parse_command_line(argv)
+        self.config=self.load_config_file(self.args.config)
 
-def load_config_file(config_file_name):
-    config=configparser.ConfigParser()
-    config['DEFAULT']={OPT_ES_TOKEN:DFLT_ES_TOKEN_FILE}
-    config[SECT_ES] = {}
-    try:
-        config.read(config_file_name)
-    except FileNotFoundError:
-        print("Config file not found, using defaults")
-    except configparser.ParsingError as e:
-        print("Config file not parsed: %s"%(e))
-        sys.exit(1)
-    return config
+    @classmethod
+    def parse_command_line(cls,argv):
+        parser = argparse.ArgumentParser(
+            prog="Energy Tariff Analysis",
+            description="Analyse energy tariffs against consumption and generation records",
+            epilog="Load records into Elasticsearch"
+        )
+        parser.add_argument('-c', '--config', default=cls.DFLT_CONF_FILE)
+        return parser.parse_args()
 
-def load_es_token(token_file_name):
-    try:
-        with open(token_file_name,'r') as token_file:
-            return token_file.read
-    except OSError as e:
-        print("Unable to load token file: %s"%(e))
-        sys.exit(2)
+    @classmethod
+    def load_config_file(cls,config_file_name):
+        config=configparser.ConfigParser()
+        config['DEFAULT']={cls.OPT_ES_TOKEN:cls.DFLT_ES_TOKEN_FILE}
+        config[cls.SECT_ES] = {}
+        try:
+            config.read(config_file_name)
+        except FileNotFoundError:
+            print("Config file not found, using defaults")
+        except configparser.ParsingError as e:
+            print("Config file not parsed: %s"%(e))
+            sys.exit(1)
+        return config
+
+    def get_es_token_filename(self):
+        return self.config.get(self.SECT_ES,self.OPT_ES_TOKEN)
+
+class ElasticsearchClient:
+
+    def __init__(self,config):
+        self.config=config
+        self.es_token=self.load_es_token(config.get_es_token_filename())
+
+    @staticmethod
+    def load_es_token(token_file_name):
+        try:
+            with open(token_file_name,'r') as token_file:
+                return token_file.read
+        except OSError as e:
+            print("Unable to load token file: %s"%(e))
+            return None
+
+    def check_connection(self):
+        if not self.es_token:
+            return False
+        return True
 
 if __name__ == '__main__':
     print('Welcome to the Energy Tariff Analysis script')
-    args=parse_command_line(sys.argv)
-    config=load_config_file(args.config)
-    es_token=load_es_token(config.get(SECT_ES,OPT_ES_TOKEN))
+    config=Configuration(sys.argv)
+    es_client=ElasticsearchClient(config)
+    es_client.check_connection()
